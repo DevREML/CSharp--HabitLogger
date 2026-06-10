38d4e9352f07c827191dbf9ad3fafe697798cf26
DevREML <raissa_limon@hotmail.com>
2026-06-10 22:27:59 +0200

    pseudocode

---
+++ Database.cs
@@ -0,0 +1,5 @@
+// create a sqlite database, if one isn't present
+
+// create a table in the database, where the habit will be logged
+
+// store and retrieve data
--- Program.cs
+++ Program.cs
@@ -1,1 +1,7 @@
 ﻿Console.WriteLine("Hello, World!");
+
+// start menu 
+
+// input the date of the occurence of the habit
+
+// Insert, delete, update and view logged habit

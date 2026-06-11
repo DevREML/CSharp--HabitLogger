f903b2e64500074ba37985725e1bde5fa7ea71b0
DevREML <raissa_limon@hotmail.com>
2026-06-11 12:30:47 +0200

    added startmenu

--- Program.cs
+++ Program.cs
@@ -1,10 +1,24 @@
 ﻿// Calling HabitsDatabase.db
 var database = new Database();
 database.CreateDatabase();
-    
 
 // start menu 
+Console.WriteLine("Welcome to the Habit Logger!");
+Console.WriteLine("Press one of the following options:\n1.Insert new habit\n2.View habits\n3.Update habits\n4.Delete habits");
+string choosenOption = Console.ReadLine();
 
-// input the date of the occurence of the habit
-
-// Insert, delete, update and view logged habit
+switch (choosenOption)
+{
+    case "1":
+        Console.WriteLine($"You have choosen {choosenOption}!");
+        break;
+    case "2":
+        Console.WriteLine($"You have choosen {choosenOption}!");
+        break;
+    case "3":
+        Console.WriteLine($"You have choosen {choosenOption}!");
+        break;
+    case "4":
+        Console.WriteLine($"You have choosen {choosenOption}!");
+        break;
+}
